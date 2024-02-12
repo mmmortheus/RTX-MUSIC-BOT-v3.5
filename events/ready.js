a56260e3e90206bfa4f0638d127cd78e69fe6b32
mmmortheus <159746249+mmmortheus@users.noreply.github.com>
2024-02-12 14:26:47 +0200

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -23,7 +23,7 @@ console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}
 
 setInterval(() => client.user.setActivity({ 
   name: `Your favorite tunes`, 
-  type: ActivityType.Playing }), 10000);
+  type: ActivityType.Listening }), 10000);
 client.errorLog = config.errorLog
 } else {
 console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`)
